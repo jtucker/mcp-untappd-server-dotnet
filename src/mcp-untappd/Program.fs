e1d9759c68a5eebdfea24d405df2f52e06cc8cc2
Jason Tucker <jtucker@gmail.com>
2025-04-07 03:01:50 +0000

    working azure function

---
+++ src/mcp-untappd/Program.fs
@@ -0,0 +1,19 @@
+﻿module McpUntappdAzFunc.Program
+
+open Microsoft.Azure.Functions.Worker.Builder
+open Microsoft.Extensions.Hosting
+
+let args = System.Environment.GetCommandLineArgs()
+
+let builder =    
+    FunctionsApplication
+        .CreateBuilder(args)                
+        .ConfigureFunctionsWebApplication()
+
+// Setup the MCP SDK Metadata
+builder.EnableMcpToolMetadata() |> ignore
+builder
+    .Build()
+    .RunAsync()
+|> Async.AwaitTask
+|> Async.RunSynchronously
