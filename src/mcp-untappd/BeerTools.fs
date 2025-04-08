87e0eead9a9b7c7625e83d4fdbbb956152b5a368
Jason Tucker <jtucker@users.noreply.github.com>
2025-04-08 00:07:54 -0400

    Get Beer by ID Tool working

--- src/mcp-untappd/BeerTools.fs
+++ src/mcp-untappd/BeerTools.fs
@@ -1,15 +1,37 @@
 namespace McpUntappdAzFunc.Functions
 
-
 open Microsoft.Azure.Functions.Worker
 open Microsoft.Extensions.Logging
 open Microsoft.Azure.Functions.Worker.Extensions.Mcp
 open McpUntappdAzFunc
+open FsHttp
+open System.Net
+open Microsoft.Extensions.Options
 
-type BeerTools (logger: ILogger<BeerTools>, untappedOptions: UntappdOptions) =
+type BeerTools (logger: ILogger<BeerTools>, untappedOptions: IOptions<UntappdOptions>) =
+
+    let untappdSettings = untappedOptions.Value
 
     [<Function(GET_BEER_INFO_FUNC_NAME)>]
     member _.GetBeerInfo(
         [<McpToolTrigger(GET_BEER_INFO_TOOL_NAME, GET_BEER_INFO_DESCRIPTION)>] toolContent: ToolInvocationContext,
         [<McpToolProperty("beer_id", "string", "The ID of the beer")>] beerId: string) =        
-        "hello"
+        task {
+            logger.LogInformation("Getting Beer information for Beer Id {BeerId}", beerId)
+
+            let! resp =
+                http {
+                    GET $"{UNTAPPD_API_URI}/beer/info/{beerId}"
+                    query [
+                        "client_id", untappdSettings.ClientId
+                        "client_secret", untappdSettings.ClientSecret 
+                    ]
+                }
+                |> Request.sendTAsync
+            
+            return!
+                match resp.statusCode with
+                | HttpStatusCode.OK -> 
+                    resp.content.ReadAsStringAsync()
+                | _ -> System.Threading.Tasks.Task.FromResult "Error retrieving the beer from Untappd"
+        }
